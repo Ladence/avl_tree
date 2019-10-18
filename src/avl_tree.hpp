faee0dd44b479030dd865e545ddbb27fea97dd42
Zakhar Karlin <zakhar.karlin11@gmail.com>
2019-10-18 16:17:25 +0300

    add docs and defined methods in avl_tree

--- src/avl_tree.hpp
+++ src/avl_tree.hpp
@@ -70,8 +70,61 @@ template <typename value_t> class avl_tree {
   };
 
 public:
-  //  AVLTree() {}
-  //  ~AVLTree() {}
+  /**
+   * Inserts node with predefined value in tree
+   * @param value value-field of insertable node
+   */
+  void insert(const value_t &value) { throw; }
+  /**
+   * Removes element with predefined value from tree
+   * @param value value-field of deletable node
+   */
+  void erase(const value_t &value) { throw; }
+
+  /**
+   * Removes all elements from tree
+   */
+  void clear() { throw; }
+
+  /**
+   * Returns is the tree doesn't contain elements
+   * @return true if no elements are present, else false
+   */
+  bool empty() const { return m_size == 0; }
+
+  /**
+   * Returns tree's actual size (count of elements or nodes in tree)
+   * @return actual size of tree
+   */
+  std::size_t size() const { return m_size; }
+
+  /**
+   * Returns position (index) in tree of element with predefined value field
+   * @param value desired value
+   * @return idx of desired element in tree
+   */
+  std::size_t find(const value_t &value) const { throw; }
+
+  /**
+   * Returns an pos of the first element in tree that is >= value
+   * @param value value to compare the elements to
+   * @return pos of the first element that is value-field >= value.
+   */
+  std::size_t lower_bound(const value_t &value) const { throw; }
+
+  /**
+   * Returns an pos of the first element in tree that is <= value
+   * @param value value to compare the elements to
+   * @return pos of the first element that is value-field <= value
+   */
+  std::size_t upper_bound(const value_t &value) const { throw; }
+
+  /**
+   * Returns mutable value (const ref) of findable node by index
+   * @param index index of element in tree
+   * @return const reference to founded element
+   */
+  const value_t &operator[](std::size_t index) const { throw; }
 
 private:
   std::size_t m_size;
